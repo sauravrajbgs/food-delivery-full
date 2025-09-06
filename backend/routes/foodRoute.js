527841010d02b8ea3df081fe3a24fed7ef18e6de
saurav kumar <sauravkumar8887252285@gmail.com>
2025-09-06 10:44:09 +0530

    fix: switch multer to memoryStorage for Vercel

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,20 +1,35 @@
-import express from 'express';
-import { addFood , listFood ,removeFood} from '../controllers/foodController.js';
-import multer from 'multer';
+// import express from 'express';
+// import { addFood , listFood ,removeFood} from '../controllers/foodController.js';
+// import multer from 'multer';
 
-const foodRouter=express.Router();
+// const foodRouter=express.Router();
 
-const storage=multer.diskStorage({
-    destination:"uploads",
-    filename:(req ,file,cb)=>{
-return cb(null, `${Date.now()} ${file.originalname}`);
-    }
-})
-const upload=multer({storage:storage})
+// const storage=multer.diskStorage({
+//     destination:"uploads",
+//     filename:(req ,file,cb)=>{
+// return cb(null, `${Date.now()} ${file.originalname}`);
+//     }
+// })
+// const upload=multer({storage:storage})
 
-foodRouter.post("/add", upload.single("image"),addFood)
-foodRouter.get("/list",listFood)
-foodRouter.post('/remove',removeFood)    
+// foodRouter.post("/add", upload.single("image"),addFood)
+// foodRouter.get("/list",listFood)
+// foodRouter.post('/remove',removeFood)    
 
+
+// export default foodRouter;
+
+import express from "express";
+import { addFood, listFood, removeFood } from "../controllers/foodController.js";
+import multer from "multer";
+
+const foodRouter = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list", listFood);
+foodRouter.post("/remove", removeFood);
 
 export default foodRouter;
